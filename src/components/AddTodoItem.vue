d1f853605181e17d60d2de62ab88433b40f8b9ff
ming <xiemingzhi@gmail.com>
2020-01-13 10:29:31 +0800

    A VueJs Ionic Framework Example

---
+++ src/components/AddTodoItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <ion-page>
+    <ion-header class="toolbar-md-primary">
+
+      <ion-toolbar>
+        <ion-title>Add Item</ion-title>
+      </ion-toolbar>
+
+    </ion-header>
+    <ion-content class="content">
+
+      <ion-item>
+        <ion-input :value="name" ref="newTodoItem" @input="updateTodoName" placeholder="Todo Name"></ion-input>
+      </ion-item>
+
+      <ion-fab-button class="todo-fab" @click="addTodo">
+        <ion-icon name="checkmark"></ion-icon>
+      </ion-fab-button>
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      const newTodo = { name: this.name }
+      axios.post('https://jsonplaceholder.typicode.com/todos', newTodo)
+        .then(res => {
+          this.$router.push({path: '/todos'})
+        })
+    },
+    updateTodoName() {
+      this.name = this.$refs.newTodoItem.value
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,56 @@
+<template>
+  <ion-page>
+    <ion-header>
+
+      <ion-toolbar class="toolbar-md-primary">
+        <ion-title>TodoList</ion-title>
+      </ion-toolbar>
+
+    </ion-header>
+    <ion-content class="content">
+
+      <ion-list>
+        <ion-item v-for="todo in todos" :key="todo.id">
+          {{todo.title}}
+        </ion-item>
+      </ion-list>
+
+      <ion-fab-button class="todo-fab" @click="addTodo">
+        <ion-icon name="add"></ion-icon>
+      </ion-fab-button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      this.$router.push({path: '/todos/add'})
+    }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.todos = res.data)
+  }
+}</script>
+
+<style lang="scss" scoped>
+.todo-fab {
+  position: fixed;
+  bottom: 25px;
+  right: 15px;
+  font-size: 30px;
+}
+
+.content {
+  padding: 10px 10px 10px 0px
+}
+</style>
